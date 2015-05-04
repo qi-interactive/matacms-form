68d49f4495bca0e483513e458cddcdd79cf2c0dc
Michal Fiedorowicz <michal@qi-interactive.com>
2015-05-04 19:59:00 +0100

    Missing ModuleAsset added

---
+++ assets/ModuleAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace matacms\form\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ModuleAsset extends AssetBundle {
+	public $sourcePath = '@vendor/matacms/matacms-form/web';
+}
