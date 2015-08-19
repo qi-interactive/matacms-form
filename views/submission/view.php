b715561c57d49487e846c9cbf10d4a51d8249f68
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-08-19 17:31:30 +0100

    Updates to markups
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- views/submission/view.php
+++ views/submission/view.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use yii\helpers\Inflector;
+use matacms\theme\simple\assets\ModuleIndexAsset;
 
 /* @var $this yii\web\View */
 /* @var $model mata\contentblock\models\ContentBlock */
@@ -14,29 +15,29 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="module-entry-detail-view">
 
-<div class="content-block-view">
+    <div class="content-block-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'template' => '<div class="item row"><div class="two columns">{label}</div><div class="eight columns">{value}</div></div>',
-        'options' => [
+        <?= DetailView::widget([
+            'model' => $model,
+            'template' => '<div class="item row"><div class="three columns item-label">{label}</div><div class="nine columns info">{value}</div></div>',
+            'options' => [
             'tag' => 'div',
             'class' => 'details-view'
-        ]
-    ]) ?>
+            ]
+            ]) ?>
 
-</div>
-</div>
+        </div>
+    </div>
 
-<script>
+    <script>
 
-    parent.mata.simpleTheme.header
-    .setBackToListViewURL("<?= sprintf("/mata-cms/%s/%s/list?id=%s", $this->context->module->id, $this->context->id, $formModel->Id) ?>")
-    .setText('PREVIEW <?= Inflector::camel2words($formModel->Name) ?> FORM SUBMISSION: <?= $model->getEventDate() ?>')
-    .showBackToListView()
-    .hideVersions()
-    .show();
+        parent.mata.simpleTheme.header
+        .setBackToListViewURL("<?= sprintf("/mata-cms/%s/%s/list?id=%s", $this->context->module->id, $this->context->id, $formModel->Id) ?>")
+        .setText('PREVIEW <?= Inflector::camel2words($formModel->Name) ?> FORM SUBMISSION: <?= $model->getEventDate() ?>')
+        .showBackToListView()
+        .hideVersions()
+        .show();
 
-</script>
+    </script>
